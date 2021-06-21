e27928ded74e356bdede4f3ddedeaf9040f8af05
Kritika Kumari <70713639+Kritika10oct@users.noreply.github.com>
2021-06-21 08:27:29 +0530

    Create String_with_space.java

---
+++ JAVA/String_with_space.java
@@ -0,0 +1,11 @@
+/*Write a program which display a stringwith a space after each character?*/
+class main{
+    public static void main(String[] args) {
+       String str = "ab cd";
+       for (int i=0;i<=str.length()-1;i++){
+           System.out.print(str.charAt(i) +" ");
+
+       }
+        
+    } 
+}
