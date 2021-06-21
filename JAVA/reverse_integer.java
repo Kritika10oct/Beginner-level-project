48321fd15cf36de098837219936534ecd8e14961
Kritika Kumari <70713639+Kritika10oct@users.noreply.github.com>
2021-06-21 13:46:37 +0530

    Create reverse_integer.java

---
+++ JAVA/reverse_integer.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+class main{
+    public static void main(String[] args) {
+        int[] numbers={1,2,3,5};
+        System.out.println(numbers);
+        for(int i=numbers.length-1;i>=0;i--){
+            System.out.println(numbers[i] + " ");
+        }
+
+
+    }
+}
