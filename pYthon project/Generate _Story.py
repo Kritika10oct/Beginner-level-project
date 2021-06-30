f50ef366a18c95a80d1ab4d2a04c1ab65ada222e
Kritika Kumari <70713639+Kritika10oct@users.noreply.github.com>
2021-06-30 09:49:16 +0530

    Create Generate _Story.py

---
+++ pYthon project/Generate _Story.py
@@ -0,0 +1,6 @@
+import random
+when=['After a long time','Yesterday','After few years ago','Today']
+who=['Mohit','Rohit','Kishan','Kritika']
+where=['Newyork','india','Germany','England']
+what=['Found a Secret Kry','Eats burgur','Solved Programming Questions','Solved mistry']
+print(random.choice(when)+','+random.choice(who)+','+random.choice(where)+','+random.choice(what))
